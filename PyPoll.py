2276a9902e8b8a4baed538cf745a04b023588457
ras52017 <ras52017@yahoo.co.uk>
2022-09-16 14:22:20 -0500

    message

---
+++ PyPoll.py
@@ -0,0 +1,32 @@
+# Add our dependencies.
+import csv
+from email import header
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("..","Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("..","analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    #To do:read and analyze the data here.
+
+    file_reader = csv.reader(election_data)
+
+    #print the header row.
+    headers = next(file_reader)
+    print(headers)
+    #for row in file_reader:
+        #print(row)
+
+
+    #reader = csv.reader(election_data)
+    #print(reader)
+    # print(election_data)
+    # To do: read and analyze the data here.
+#with open(file_to_save,"w") as text_file:
+    #Write some data to the file.
+    #text_file.write("Counties in the Election\n-----------------------------------\nArapahoe\nDenver\nJefferson")
+
+
